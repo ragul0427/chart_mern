3b94b5265258c2a19cfa3989e7e679fdcc4147fc
ragul0427 <ragulhp2704@gmail.com>
2024-01-24 17:53:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Button, TextField, Box } from "@mui/material";
+import "./App.css";
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
 
 function App() {
+  const [socket, setSocket] = useState(null);
+  const [message, setMessage] = useState("");
+  const [chat, setChat] = useState([]);
+
+  useEffect(() => {
+    setSocket(io("http://localhost:4000"));
+  }, []);
+
+  useEffect(() => {
+    if (!socket) return;
+    socket.on("message-from-server", (data) => {
+      setChat([...chat, data]);
+    });
+  }, [socket]);
+
+  const handleForm = (e) => {
+    e.preventDefault();
+    socket.emit("send-message", message);
+    setMessage("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Box component="form" onSubmit={handleForm}>
+        <TextField
+          value={message}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+          defaultValue="Small"
+          size="small"
+          label="Write your message"
+          variant="standard"
+        />
+        <Button variant="text" type="submit">
+          Send
+        </Button>
+      </Box>
     </div>
   );
 }
